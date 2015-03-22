53acb1143edc888a33590230d06f559f6e219112
Graham <graham_howden1@yahoo.co.uk>
2015-03-22 19:22:45 +0000

    fix(Markdown): turns out wrapping link_attributes isn't needed as options.target is read

--- app/parsers/MarkdownParser.js
+++ app/parsers/MarkdownParser.js
@@ -6,22 +6,7 @@ import {dataLinkAddress} from '../config';
 var Parser = Snuownd.getParser();
 
 /**
- * Creates a new link_attributes function that adds target="_blank" and then calls the wrapped version if it exists
- *
- * @param {Function} original
- * @returns {Function}
- */
-var wrapLinkAttributes = function(original) {
-  return function(out, link, options) {
-    out.s += ' target="_blank"';
-
-    // call the original if it exists
-    if (original) original(out, link, options);
-  }
-};
-
-/**
- * Creates a new callback function that wraps the original but modifies the link_attributes to add target=_blank
+ * Creates a new callback function that wraps the original but modifies the options to add target=_blank
  *
  * @param {Function} original
  */
@@ -36,11 +21,8 @@ var wrapCallback = function(original) {
       return 1;
     }
 
-    // add our own link attributes
-
-    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-    options.link_attributes = wrapLinkAttributes(options.link_attributes);
-    // jscs:enable requireCamelCaseOrUpperCaseIdentifiers
+    // add target to the options
+    options.target = '_blank';
 
     // call the original
     return original(out, link, title, content, options);
